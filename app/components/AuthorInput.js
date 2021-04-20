28524d4c8ad03e43b6b756f23349625878c8a221
Jake <78624651+jakecolder93@users.noreply.github.com>
2021-04-20 13:02:05 +0100

    Update AuthorInput.js
    Update so that the url generated from user search can be used in external files.

--- app/components/AuthorInput.js
+++ app/components/AuthorInput.js
@@ -1,62 +1,41 @@
-import React,{useState, useRef, useEffect} from 'react'
+import React,{useState, useRef} from 'react'
 import '../styles/main.scss'
 
-function AuthorInput(){
-    var apiKey = "&api-key=PVnPqWozx1UIW1lsm6IEAHjyh4qxDKJR";
-    var queryAuthor = "https://api.nytimes.com/svc/books/v3/reviews.json?author=";
-
-    const [searchValue, setSearchValue] = useState("")
-    const searchField = useRef(null)
+function AuthorInput({fetchBooksList }){
     
+    const [searchValue, setSearchValue] = useState("");
+    const searchField = useRef(null);
+
+    function getURL(){
+        const urlSearchValue = encodeURIComponent(searchValue.trim()); 
+        const userURL = `https://api.nytimes.com/svc/books/v3/reviews.json?author=${urlSearchValue}&api-key=PVnPqWozx1UIW1lsm6IEAHjyh4qxDKJR`
+        return userURL  
+    } 
 
     function handleSubmit(e){
-        e.preventDefault()
-        searchField.current.focus()
-
-        //Convert user input into a URL string//
-        var urlSearchValue = encodeURIComponent(searchValue.trim()) 
-        userQuery = queryAuthor + urlSearchValue + apiKey
-        setSearchValue("")
-    }
-
+        e.preventDefault();
+        searchField.current.focus();
+        fetchBooksList(getURL());
+        setSearchValue("");
+    } 
     
-    
-    /*Fetch the data from the NYT API//
-    const useFetch = url => {
-        const [data, setData] = useState(null)
-
-        useEffect(async () => {
-            const response = await fetch(url);
-            const data = await response.json();
-            const [item] = data.results;
-            setData(item);
-        }, [])
-        return{data}
-    }
-
-    data = useFetch(userQuery)
-    console.log(data.results) */
     return(
         <>
             <div className = "author-search">
                 <form onSubmit={handleSubmit} action = "">
                     <label>Enter authors name:</label><br></br>
                     <input 
-                    ref={searchField}
+                    ref={searchField}  
                     value={searchValue}
-                    onChange = {(e) => setSearchValue(e.target.value)}
+                    onChange ={(e) => setSearchValue(e.target.value)}  
                     type = "text"
                     placeholder = "e.g. David Mitchell"
                     />
                     <button>Submit</button>
-
                 </form>
-
             </div>
         </>
     )
-} 
-
-
+}
 
 export default AuthorInput
