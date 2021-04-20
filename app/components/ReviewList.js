11f3bc7dcba4362e0748eaa482a90e00ac90283e
Jake <78624651+jakecolder93@users.noreply.github.com>
2021-04-20 13:03:54 +0100

    Update ReviewList.js
    Initial setup to check the fetch request is working from app.js

---
+++ app/components/ReviewList.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import '../styles/main.scss'
+
+
+function ReviewList({booksList}){
+
+    console.log(booksList)
+
+
+    return(
+        <>
+            <div>
+
+            </div>
+        </>
+    )
+}
+
+
+export default ReviewList
