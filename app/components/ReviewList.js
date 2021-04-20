0de7cafa993784eaad5d0a6bd5c12abdf2387593
Jake <78624651+jakecolder93@users.noreply.github.com>
2021-04-20 19:15:46 +0100

    Update ReviewList.js

--- app/components/ReviewList.js
+++ app/components/ReviewList.js
@@ -1,20 +1,19 @@
 import React from 'react'
 import '../styles/main.scss'
 
-
 function ReviewList({booksList}){
-
-    console.log(booksList)
-
-
+        
     return(
         <>
-            <div>
-
-            </div>
+            <ul>
+                {
+                    booksList.results && booksList.results.map(function(el,index) {     //'booksList.results &&' provides a check so that the .map() doesn't occur on the initial render
+                        return <li key={index}><a href = {el.url}>{el.book_title}</a><small> review by {el.byline}</small></li>
+                    })
+                }
+            </ul>
         </>
     )
 }
-
 
 export default ReviewList
